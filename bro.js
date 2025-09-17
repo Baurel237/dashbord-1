8507d9e21dcb1de844cdf2ddb2aaa361eb5b0239
baurel <baurelbemmo@gmail.com>
2025-09-17 09:27:50 +0100

    Premier commit : ajout du projet

---
+++ bro.js
@@ -0,0 +1,18 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.classList.remove('active');
+});
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+});
